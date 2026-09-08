4748907f30184923e921b5204a7c6669f1c9523d
nam <22130173@st.hcmuaf.edu.vn>
2026-09-08 14:03:27 +0700

    xóa rác

--- scripts/print_langs.mjs
+++
@@ -1,4 +0,0 @@
-import fs from 'fs';
-
-const content = fs.readFileSync('scripts/index.bundle.js', 'utf8');
-console.log(content.slice(157500, 158500));
--- scripts/show-nav-tabs.mjs
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-
-const html = fs.readFileSync('scripts/general_config_inner.html', 'utf-8');
-
-const navTabsPos = html.indexOf('nav-tabs');
-console.log('navTabsPos:', navTabsPos);
-if (navTabsPos !== -1) {
-  console.log(html.slice(navTabsPos - 100, navTabsPos + 1500));
-}
